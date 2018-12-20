f88968be68c49f82aeb47ba771a2afb27c9f23fb
Nat-Reid <nathanielreid@gmail.com>
2018-12-20 15:43:16 -0800

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, programming_language_being_learned)
+  puts "Hi, my name is #{name}, and I am learning to program in #{programming_language_being_learned}."
+end
