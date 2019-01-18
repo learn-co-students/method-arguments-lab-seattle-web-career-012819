503d0086e958f23344e6a3a32eae60175b0a6226
yong liang <yongliang24@gmail.com>
2019-01-18 21:37:45 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction (name)
+puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language (name1, name2)
+  puts "Hi, my name is #{name1} and I am learning to program in #{name2}."
+end
